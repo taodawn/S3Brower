3696cf48a37095d1fe74e0d31c58693f908a5775
taodawn <taodawn@gmail.com>
2018-11-01 08:15:32 +0800

    init

---
+++ src/main/java/br/com/trustsystems/elfinder/configuration/jaxb/ObjectFactory.java
@@ -0,0 +1,95 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2015 - 2018 Trustsystems Desenvolvimento de Sistemas, LTDA.
+ * %%
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Trustsystems Desenvolvimento de Sistemas, LTDA. nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package br.com.trustsystems.elfinder.configuration.jaxb;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the br.com.trustsystems.elfinder.configuration.jaxb package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: br.com.trustsystems.elfinder.configuration.jaxb
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ElfinderConfiguration }
+     * 
+     */
+    public ElfinderConfiguration createElfinderConfiguration() {
+        return new ElfinderConfiguration();
+    }
+
+    /**
+     * Create an instance of {@link ElfinderConfiguration.Volume }
+     * 
+     */
+    public ElfinderConfiguration.Volume createElfinderConfigurationVolume() {
+        return new ElfinderConfiguration.Volume();
+    }
+
+    /**
+     * Create an instance of {@link ElfinderConfiguration.Thumbnail }
+     * 
+     */
+    public ElfinderConfiguration.Thumbnail createElfinderConfigurationThumbnail() {
+        return new ElfinderConfiguration.Thumbnail();
+    }
+
+    /**
+     * Create an instance of {@link ElfinderConfiguration.Volume.Constraint }
+     * 
+     */
+    public ElfinderConfiguration.Volume.Constraint createElfinderConfigurationVolumeConstraint() {
+        return new ElfinderConfiguration.Volume.Constraint();
+    }
+
+}
